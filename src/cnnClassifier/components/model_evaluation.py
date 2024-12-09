dc5f23eaa19f703147dd79ab1e62d8084bef5388
kulkarnisunil <sunilkulkarni603@gmail.com>
2024-12-09 10:24:25 +0530

    updated

--- src/cnnClassifier/components/model_evaluation.py
+++ src/cnnClassifier/components/model_evaluation.py
@@ -54,10 +54,13 @@ class Evaluation:
 
     
     def log_into_mlflow(self): 
+        
+        mlflow.set_registry_uri(self.config.mlflow_uri)
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         # Set a valid tracking URI 
-        mlflow.set_tracking_uri("https://dagshub.com/sunilkulkarni603/Chest-Disease-Classification-from-chest-CT-Scan-Image.mlflow") 
+        # mlflow.set_tracking_uri("https://dagshub.com) 
         # Update this path as needed 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme 
+        
         with mlflow.start_run(): 
             mlflow.log_params(self.config.all_params) 
             mlflow.log_metrics( {"loss": self.score[0], "accuracy": self.score[1]} ) 
